058982633c900dacfb15d6f13aaab3a87c3f4dde
Devon Blandin <dblandin@gmail.com>
2021-02-08 16:43:15 -0500

    feat: respond to cli commands
    - Include @artsy/cli in package.json so we can automate version bumps via
      renovate.
    - Hardcode bot user ID for now
    
    ```
    me: @joule cli hello
    @joule: hello world from ./src/commands/hello.ts
    ```

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const { App } = require('@slack/bolt');
+const { parse } = require('shell-quote');
+const { execFile } = require('child_process');
 
 require('dotenv').config()
 
@@ -6,6 +8,20 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
+
+app.message(/^<@U01LVDNCZQF> cli (\S.*)$/, async ({ context, say }) => {
+  const args = context.matches[1];
+  const parsedArgs = parse(args);
+  const commandArgs = ["run", "--silent", "artsy"].concat(parsedArgs)
+
+  execFile("yarn", commandArgs, async (error, stdout) => {
+    if (error) {
+      await say(`error: ${error.toString()}`) ;
+    } else {
+      await say("```\n" + stdout.trim() + "\n```");
+    }
+  });
+})
 
 app.message(/^<@U01LVDNCZQF>\s(hi|hello|hey).*/i, async ({ context, say }) => {
   const greeting = context.matches[0];
