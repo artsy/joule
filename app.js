dea8db50c446f6e30ac1c8f5b81149d781ea7610
Devon Blandin <dblandin@gmail.com>
2021-02-15 13:56:17 -0500

    chore: cleanup app.js
    - Use `directMention()` builtin instead of hardcoding bot ID
    - Support DMs without @-mention
    - Extract runCLI function

--- app.js
+++ app.js
@@ -1,54 +1,72 @@
-const { App } = require('@slack/bolt');
-const { parse } = require('shell-quote');
-const { execFile } = require('child_process');
+const { App, directMention} = require('@slack/bolt')
+const { parse } = require('shell-quote')
+const { execFile } = require('child_process')
 
 require('dotenv').config()
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
-});
+})
 
-app.message(/^<@U01LVDNCZQF> cli (\S.*)$/, async ({ context, say }) => {
-  const args = context.matches[1];
-  const parsedArgs = parse(args);
+async function onlyDirectMessages({ event, message, next }) {
+  if (event.channel_type === "im") {
+    await next()
+  }
+}
+
+async function runCLI(args, say) {
+  const parsedArgs = parse(args)
   const commandArgs = ["run", "--silent", "artsy"].concat(parsedArgs)
 
   execFile("yarn", commandArgs, async (error, stdout) => {
     if (error) {
-      await say(`error: ${error.toString()}`) ;
+      return error.toString()
     } else {
-      await say("```\n" + stdout.trim() + "\n```");
+      return stdout
     }
-  });
-})
+  })
+}
 
-app.message(/^<@U01LVDNCZQF>\s(hi|hello|hey).*/i, async ({ context, say }) => {
-  const greeting = context.matches[0];
+async function processCLICommand({ context, say }) {
+  const args = context.matches.groups.args
+  const result = await runCLI(args)
+  
+  await say("```\n" + result.trim() + "\n```")
+}
 
-  await say(`${greeting}, how are you?`);
-});
+async function processGreeting({ context, say }) {
+  const greeting = context.matches.groups.greeting
+
+  await say(`${greeting}, how are you?`)
+}
+
+app.message(onlyDirectMessages, /^cli (?<args>\S.*)$/, processCLICommand)
+app.message(directMention(), /^<@U\S+> cli (?<args>\S.*)$/, processCLICommand)
+
+app.message(onlyDirectMessages, /^(?<greeting>hi|hello|hey).*/i, processGreeting)
+app.message(directMention(), /^<@U\S+> (?<greeting>hi|hello|hey).*/i, processGreeting)
 
 if (process.env.DEBUG) {
   app.use(args => {
-    const copiedArgs = JSON.parse(JSON.stringify(args));
-    copiedArgs.context.botToken = 'xoxb-***';
+    const copiedArgs = JSON.parse(JSON.stringify(args))
+    copiedArgs.context.botToken = 'xoxb-***'
     if (copiedArgs.context.userToken) {
-      copiedArgs.context.userToken = 'xoxp-***';
+      copiedArgs.context.userToken = 'xoxp-***'
     }
-    copiedArgs.client = {};
-    copiedArgs.logger = {};
+    copiedArgs.client = {}
+    copiedArgs.logger = {}
     args.logger.info(
       "Dumping request data for debugging...\n\n" +
       JSON.stringify(copiedArgs, null, 2) +
       "\n"
-    );
-    args.next();
+    )
+    args.next()
   });
 }
 
 (async () => {
-  await app.start(process.env.PORT || 3000);
+  await app.start(process.env.PORT || 3000)
 
-  console.log('⚡️ Bolt app is running!');
-})();
+  console.log('⚡️ Bolt app is running!')
+})()
